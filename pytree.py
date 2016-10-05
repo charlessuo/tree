b9c19aafc61b072321f1a7c5b2d197227b0dad08
Charles Suo <Charles@dhcp-ccc-7483.eduroam.cornell.edu>
2016-10-05 16:57:01 -0400

    third trial

--- pytree.py
+++ pytree.py
@@ -1,44 +1,66 @@
 #!/usr/bin/env python3
-
 import sys
-import re
 import os
+import re
 import string
-from collections import defaultdict
 
 
-def file_tree(path, count, t):
-    padding0 = '    '
-    paddingT = '├── '
-    paddingI = '│   '
-    paddingL = '└── '
-    space = t
-    pathlist = os.listdir(path)
-    for i, files in enumerate(pathlist):
-        temppath = path + os.sep + files
-        if i == len(pathlist) - 1:
-            child = space + paddingL + files
+paddingI = "│   "
+padding0 = "    "
+paddingT = "├── "
+paddingL = "└── "
+
+
+def prepare_path(path):
+    children = os.listdir(path)
+    for t in children:
+        if t[0] != '.':
+            children.append(t)
+    children.sort()
+    return children
+
+
+def prepare_dir(new_path, counts, the_end, padding):
+    counts[0] = counts[0] + 1
+    if (the_end):
+        padding = padding + padding0
+    else:
+        padding = padding + paddingI
+    print_tree(new_path, counts, padding)
+    padding = padding[:-4]
+
+
+def print_tree(path, counts, padding=""):
+    children = prepare_path(path)
+    for i in range(0, len(children)):
+        the_end = False
+        if (i == len(children) - 1):
+            the_end = True
+            print(padding + paddingL + children[i])
         else:
-            child = space + paddingT + files
-        print (child)
-        count[1] = count[1] + 1
-        if os.path.isdir(temppath):
-            space = space + paddingI
-            file_tree(temppath, count, space)
-            count[0] = count[0] + 1
+            print(padding + paddingT + children[i])
+        new_path = os.path.join(path, children[i])
+        if (os.path.isdir(new_path)):
+            prepare_dir(new_path, counts, the_end, padding)
+        else:
+            counts[1] = counts[1] + 1
 
-if __name__ == '__main__' :
-  if (len(sys.argv) == 1):
-    dirname = "."
-    count = [0, 0]
-  elif (len(sys.argv) == 2):
-    dirname= sys.argv[1]
-    count = [0, 0]
-  else:
-    print ('Invalid')
-    sys.exit()
-  print (dirname)
-  space = ''
-  file_tree(dirname, count, space)
-  print ()
-  print ('%d directories, %d files' % (count[0], count[1]-count[0]))
+
+if __name__ == '__main__':
+    if (len(sys.argv) > 2):
+        print("Invalid")
+        sys.exit()
+    counts = [0, 0]
+    path = "."
+    if len(sys.argv) == 2:
+        path = sys.argv[1]
+    print(path)
+    print_tree(path, counts)
+    print()
+    dir_str = "directories"
+    file_str = "files"
+    if (counts[0] == 1):
+        dir_str = "directory"
+    if (counts[1] == 1):
+        file_str = "file"
+    print("{} {}, {} {}".format(counts[0], dir_str, counts[1], file_str))
